8569cfc2b8c4a9ba3f7faac8d41f5a44456d4ec4
wavepharanyu <pharanyu.s@ku.th>
2024-09-21 13:45:43 +0700

    edit: edit navbar link and cart quantity

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import { assets } from "../../assets/assets"
 import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
+import { HashLink } from "react-router-hash-link";
 
 const Navbar = ({setShowLogin}) => {
 
@@ -20,10 +21,10 @@ const Navbar = ({setShowLogin}) => {
     <div className="navbar">
         <Link to='/'><h1 className="name">Food-Del</h1></Link>
         <ul className="navbar-menu">
-            <Link to="/" onClick={() => setMenu("home")} className={menu==="home" ? "active" : ""}>Home</Link>
-            <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu==="menu" ? "active" : ""}>Menu</a>
-            <a href="#app-download" onClick={() => setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>Mobile app</a>
-            <a href="#footer" onClick={() => setMenu("contact-us")} className={menu==="contact-us" ? "active" : ""}>Contact Us</a>
+            <HashLink to="/" onClick={() => setMenu("home")} className={menu==="home" ? "active" : ""}>Home</HashLink>
+            <HashLink to="/#explore-menu" onClick={() => setMenu("menu")} className={menu==="menu" ? "active" : ""}>Menu</HashLink>
+            <HashLink to="/#app-download" onClick={() => setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>Mobile app</HashLink>
+            <HashLink to="/#footer" onClick={() => setMenu("contact-us")} className={menu==="contact-us" ? "active" : ""}>Contact Us</HashLink>
         </ul>
         <div className="navbar-right">
             <div className="navbar-search-icon">
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems, foodList, removeFromCart, getTotalCartAmount, url } = useContext(StoreContext);
+  const { cartItems, foodList, removeFromCart, getTotalCartAmount, url, addToCart } = useContext(StoreContext);
 
   const navigate = useNavigate()
   return (
@@ -28,7 +28,7 @@ const Cart = () => {
                   <img src={`${url}/images/${item.image}`} alt="" />
                   <p>{item.name}</p>
                   <p>฿{item.price}</p>
-                  <p>{cartItems[item._id]}</p>
+                  <div className="cart-items-quantity"><button onClick={() => {removeFromCart(item._id)}} className="cart-items-quantity-btn">-</button><span className="cart-items-quantity-text">{cartItems[item._id]}</span><button onClick={() => {addToCart(item._id)}} className="cart-items-quantity-btn">+</button></div>
                   <p>฿{item.price * cartItems[item._id]}</p>
                   <p onClick={() => removeFromCart(item._id)} className="cross">x</p>
                 </div>
